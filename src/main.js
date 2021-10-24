3a0becf11dc4c721dedca4a7ad2e6ad46522c14c
= <tmgast@gmail.com>
2021-10-25 08:15:40 +0900

    threejs samples added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+// import * as VueThreejs from '@/../package/lib/VueThreejs.common' // testing pack
+// import * as VueThreejs from '@/../lib/VueThreejs.common' // testing build
+import * as VueThreejs from '@'
+// import VueThreejs from '@'
+Vue.use(VueThreejs)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
